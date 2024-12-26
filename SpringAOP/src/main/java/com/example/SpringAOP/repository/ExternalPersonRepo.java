e0a0f6b7334387cbe1997ad429f790398d32b04d
Alexandr <a-koshkarov@mail.ru>
2024-12-26 23:14:03 +0500

    ResultHW12

--- SpringAOP/src/main/java/com/example/SpringAOP/repository/ExternalPersonRepo.java
+++ SpringAOP/src/main/java/com/example/SpringAOP/repository/ExternalPersonRepo.java
@@ -1,7 +1,9 @@
 package com.example.SpringAOP.repository;
 
 import com.example.SpringAOP.model.ExternalPerson;
+import com.example.SpringAOP.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExternalPersonRepo {
+public interface ExternalPersonRepo extends JpaRepository<Person, Long> {
     ExternalPerson getExternalPerson(Long id);
 }
